080a76a42b7f522de1f179ce2e31291ca2994343
winterMute84 <wcarbajal84@gmail.com>
2018-01-14 17:03:11 -0800

    finished the prototype for index.html

---
+++ js/footer.js
@@ -0,0 +1,17 @@
+function emailPopulate()
+{
+    document.querySelector("footer").innerHTML='<p style="font-size:4vmin;'+ 
+    'color:rgb(172, 155, 63);'+ 
+    'margin-top: 3vmin;">'+
+    'Email: email@someone.com <p>';
+
+}
+
+function phonePopulate()
+{
+    document.querySelector("footer").innerHTML='<p style="font-size:4vmin;'+ 
+    'color:rgb(172, 155, 63);'+ 
+    'margin-top: 3vmin;">'+
+    'Contact: (213) 123-4567<p>';
+
+}
