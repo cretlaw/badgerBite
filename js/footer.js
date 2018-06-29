b8c1caa727ef7e0dcc2dbf3acca09cd74fe94488
winterMute84 <wcarbajal84@gmail.com>
2018-06-28 23:10:06 -0700

    Changes made at coffe store- Linked social networks/added events

--- js/footer.js
+++ js/footer.js
@@ -3,7 +3,7 @@ function emailPopulate()
     document.querySelector("footer").innerHTML='<p style="font-size:4vmin;'+ 
     'color:whitesmoke;'+ 
     'margin-top: 3vmin;">'+
-    'Email: email@someone.com <p>';
+    'Email: badgerclawcuts@gmail.com <p>';
 
 }
 
@@ -12,6 +12,6 @@ function phonePopulate()
     document.querySelector("footer").innerHTML='<p style="font-size:4vmin;'+ 
     'color:whitesmoke;'+ 
     'margin-top: 3vmin;">'+
-    'Contact: (213) 123-4567<p>';
+    'Contact: (323) 314-3768<p>';
 
 }
