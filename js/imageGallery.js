a544e06400f85c7493c67deede0ece06c53ac307
winterMute84 <wcarbajal84@gmail.com>
2018-09-09 17:38:49 -0700

    Added new commisions page

--- js/imageGallery.js
+++ js/imageGallery.js
@@ -1,3 +1,11 @@
+// $(document).ready(function () {
+//     $(".gallery-img").click(function(){
+//       var t = $(this).attr("src");
+//       $(".modal-body").html("<img src='"+t+"' class='modal-img'>");
+//       $("#myModal").modal();
+//     });
+//   });
+
 
 $(function () {
     $('img').on('click', function () {
